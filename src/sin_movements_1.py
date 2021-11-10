e28c4f3488f681c8c214de64f9ce84e134e7d31b
Thanakrit-Anutrakulchai <tuchapoltr@gmail.com>
2021-11-10 11:11:46 +0000

    Fix joints at angle 0

--- src/sin_movements_1.py
+++ src/sin_movements_1.py
@@ -7,6 +7,9 @@ from std_msgs.msg import Float64
 # publishes to topics to tell joints to move in sinusoidal movements
 #   for Joint State Estimation I, as written in the IVR assignment pdf
 
+def joint1_movement(_):
+    return 0
+
 def joint2_movement(t):
     return np.pi/2 * np.sin(t * np.pi/15)
 
@@ -21,22 +24,26 @@ def sin_movements_1():
 
     # initialize node and publishers to joint angles
     rospy.init_node('sin_movements_1', anonymous=True)
-    joint2_pub = rospy.Publisher(pub_str % 2 , Float64, queue_size=10)
-    joint3_pub = rospy.Publisher(pub_str % 3 , Float64, queue_size=10)
-    joint4_pub = rospy.Publisher(pub_str % 4 , Float64, queue_size=10)
+    joint1_pub = rospy.Publisher(pub_str % 1, Float64, queue_size=10)
+    joint2_pub = rospy.Publisher(pub_str % 2, Float64, queue_size=10)
+    joint3_pub = rospy.Publisher(pub_str % 3, Float64, queue_size=10)
+    joint4_pub = rospy.Publisher(pub_str % 4, Float64, queue_size=10)
 
     rate = rospy.Rate(50)
     start_time = rospy.get_time()
     while not rospy.is_shutdown():
         time_from_start = rospy.get_time() - start_time
+        j1 = Float64()
         j2 = Float64()
         j3 = Float64()
         j4 = Float64() 
         
+        j1.data = joint1_movement(time_from_start)
         j2.data = joint2_movement(time_from_start)
         j3.data = joint3_movement(time_from_start)
         j4.data = joint4_movement(time_from_start)
 
+        joint1_pub.publish(j1)
         joint2_pub.publish(j2)
         joint3_pub.publish(j3)
         joint4_pub.publish(j4)
--- src/sin_movements_2.py
+++ src/sin_movements_2.py
@@ -10,6 +10,9 @@ from std_msgs.msg import Float64
 def joint1_movement(t):
     return np.pi   * np.sin(t * np.pi/28)
 
+def joint2_movement(_):
+    return 0
+
 def joint3_movement(t):
     return np.pi/2 * np.sin(t * np.pi/20)
 
@@ -21,23 +24,27 @@ def sin_movements_2():
 
     # initialize node and publishers to joint angles
     rospy.init_node('sin_movements_2', anonymous=True)
-    joint1_pub = rospy.Publisher(pub_str % 1 , Float64, queue_size=10)
-    joint3_pub = rospy.Publisher(pub_str % 3 , Float64, queue_size=10)
-    joint4_pub = rospy.Publisher(pub_str % 4 , Float64, queue_size=10)
+    joint1_pub = rospy.Publisher(pub_str % 1, Float64, queue_size=10)
+    joint2_pub = rospy.Publisher(pub_str % 2, Float64, queue_size=10)
+    joint3_pub = rospy.Publisher(pub_str % 3, Float64, queue_size=10)
+    joint4_pub = rospy.Publisher(pub_str % 4, Float64, queue_size=10)
 
     rate = rospy.Rate(50)
     start_time = rospy.get_time()
     while not rospy.is_shutdown():
         time_from_start = rospy.get_time() - start_time
         j1 = Float64()
+        j2 = Float64()
         j3 = Float64()
         j4 = Float64() 
         
         j1.data = joint1_movement(time_from_start)
+        j2.data = joint2_movement(time_from_start)
         j3.data = joint3_movement(time_from_start)
         j4.data = joint4_movement(time_from_start)
 
         joint1_pub.publish(j1)
+        joint2_pub.publish(j2)
         joint3_pub.publish(j3)
         joint4_pub.publish(j4)
         
