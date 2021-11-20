d147cf433c7ec9cbddfdad15f7b0e832a65168fa
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-11-21 00:43:24 +0530

    fixed linting

--- src/control.py
+++ src/control.py
@@ -1,12 +1,11 @@
 # calculates forward kinematics and uses inverse kinematics to move robot towards desired trajectory
 
 import sys
-import rospy
+
 import cv2
 import numpy as np
-from sensor_msgs.data import Image
-from std_msgs.data    import Float64MultiArray, Float64
-from cv_bridge       import CvBridge
+import rospy
+from std_msgs.data import Float64, Float64MultiArray
 
 LINK_1_LENGTH = 4
 LINK_3_LENGTH = 3.2
@@ -18,53 +17,63 @@ L3 = LINK_3_LENGTH
 L4 = LINK_4_LENGTH
 
 
-
 # TODO
 # forward kinematics
 # short-hand for sin and cos
 def s(a):
     return np.sin(a)
 
+
 def c(a):
     return np.cos(a)
 
 # calculate the position of the end-effector from the angles of joints 1, 3, 4
+
+
 def K(j1, j3, j4):
-    x =  c(j1)*s(j4)*L4 + s(j1)*s(j3)*c(j4)*L4 + s(j1)*s(j3)*L3
-    y =  s(j1)*s(j4)*L4 - c(j1)*s(j3)*c(j4)*L4 - c(j1)*s(j3)*L3
-    z =  c(j3)*c(j4)*L4 + c(j3)*L3             + L1
+    x = c(j1)*s(j4)*L4 + s(j1)*s(j3)*c(j4)*L4 + s(j1)*s(j3)*L3
+    y = s(j1)*s(j4)*L4 - c(j1)*s(j3)*c(j4)*L4 - c(j1)*s(j3)*L3
+    z = c(j3)*c(j4)*L4 + c(j3)*L3 + L1
     return [x, y, z]
 
 # calculate the Jacobian from joint angles
 #   the rows are "written as columns" for readability
+
+
 def get_jacobian(j1, j3, j4):
     first_row = np.array([
         -s(j1)*s(j4)*L4 + c(j1)*s(j3)*c(j4)*L4 + c(j1)*s(j3)*L3,
-         s(j1)*c(j3)*c(j4)*L4 + s(j1)*c(j3)*L3,
-         c(j1)*c(j4)*L4 - s(j1)*s(j3)*s(j4)*L4
-         ])
+        s(j1)*c(j3)*c(j4)*L4 + s(j1)*c(j3)*L3,
+        c(j1)*c(j4)*L4 - s(j1)*s(j3)*s(j4)*L4
+    ])
 
     second_row = np.array([
         c(j1)*s(j4)*L4 + s(j1)*s(j3)*c(j4)*L4 + s(j1)*s(j3)*L3,
         -c(j1)*c(j3)*c(j4)*L4 - c(j1)*c(j3)*L3,
-         s(j1)*c(j4)*L4 + c(j1)*s(j3)*s(j4)*L4
-        ])
+        s(j1)*c(j4)*L4 + c(j1)*s(j3)*s(j4)*L4
+    ])
 
     third_row = np.array([
         0,
         -s(j3)*c(j4)*L4 - s(j3)*L3,
         -c(j3)*s(j4)*L4
-        ])
+    ])
     return np.stack([first_row, second_row, third_row], axis=0)
 
 
 class Control:
     def __init__(self):
         # subscribe to relevant topics
-        self.joint_1_sub = rospy.Subscriber('joint_angle_1', Float64, self._read_joint1_angle)
-        self.joint_3_sub = rospy.Subscriber('joint_angle_3', Float64, self._read_joint3_angle)
-        self.joint_4_sub = rospy.Subscriber('joint_angle_4', Float64, self._read_joint4_angle)
-        self.target_sub = rospy.Subscriber('/target_control/target_pos', Float64MultiArray, self.open_control)
+        self.joint_1_sub = rospy.Subscriber(
+            'joint_angle_1', Float64, self._read_joint1_angle)
+        self.joint_3_sub = rospy.Subscriber(
+            'joint_angle_3', Float64, self._read_joint3_angle)
+        self.joint_4_sub = rospy.Subscriber(
+            'joint_angle_4', Float64, self._read_joint4_angle)
+        self.detected_end_effector_sub = rospy.Subscriber(
+            'end_effector_pos', Float64MultiArray, self._read_detected_end_effector_position)
+        self.target_sub = rospy.Subscriber(
+            '/target_control/target_pos', Float64MultiArray, self.open_control)
 
         # prepare publishers for joint angles
         self.joint_1_pub = rospy.Publisher(
@@ -117,7 +126,7 @@ class Control:
         joint_angels = [self._detected_q['join1'],
                         self._detected_q['join3'],
                         self._detected_q['join4']]
-        
+
         q = np.array([*joint_angels])
         J = get_jacobian(*joint_angels)
         J_pinv = np.linalg.pinv(J)
@@ -136,12 +145,13 @@ class Control:
 
 
 def main(args):
-  _ = Control()
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
+    _ = Control()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main(sys.argv)
