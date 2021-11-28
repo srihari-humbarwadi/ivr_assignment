092c8164c8cac7682ac1cf39a7e11ee2a8379904
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-11-28 21:12:29 +0530

    fixed linting

--- src/control.py
+++ src/control.py
@@ -29,6 +29,8 @@ def c(a):
     return tf.math.cos(a)
 
 # calculate the position of the end-effector from the angles of joints 1, 3, 4
+
+
 def FK(j1, j3, j4):
     x = c(j1)*s(j4)*L4 + s(j1)*s(j3)*c(j4)*L4 + s(j1)*s(j3)*L3
     y = s(j1)*s(j4)*L4 - c(j1)*s(j3)*c(j4)*L4 - c(j1)*s(j3)*L3
@@ -37,6 +39,8 @@ def FK(j1, j3, j4):
 
 # calculate the Jacobian from joint angles
 #   the rows are "written as columns" for readability
+
+
 def get_jacobian(j1, j3, j4):
     first_row = np.array([
         -s(j1)*s(j4)*L4 + c(j1)*s(j3)*c(j4)*L4 + c(j1)*s(j3)*L3,
@@ -60,6 +64,7 @@ def get_jacobian(j1, j3, j4):
 
 optimizer = tf.optimizers.SGD(learning_rate=0.01)
 
+
 @tf.function(jit_compile=True)
 def solve(target_pos, j1, j3, j4):
     q = [j1, j3, j4]
@@ -67,11 +72,11 @@ def solve(target_pos, j1, j3, j4):
     # compiled tf graph requires us to initialize all outputs
     error = tf.constant(0.0)
 
-    # run gradient descent for 500 steps 
+    # run gradient descent for 500 steps
     # (in most of the cases we find a solution within 100 steps)
     for step in tf.range(1, 501):
         with tf.GradientTape() as tape:
-            # compute current position of the 
+            # compute current position of the
             # end effector using forward kinematics
             current_pos = FK(j1, j3, j4)
 
@@ -79,10 +84,10 @@ def solve(target_pos, j1, j3, j4):
             # and target positions of the end effector
             error = tf.reduce_mean((target_pos - current_pos) ** 2)
 
-            # Use automatic differentiation to compute 
+            # Use automatic differentiation to compute
             # gradients d(error)/dq. Since we are using
             # forward kinematics to compute the current
-            # position, we can show that the gradients 
+            # position, we can show that the gradients
             # can be expressed in terms of the Jacobian matrix.
             gradients = tape.gradient(error, q)
 
@@ -97,16 +102,19 @@ def solve(target_pos, j1, j3, j4):
         optimizer.apply_gradients(zip(gradients, q))
     return {'steps': step, 'error': mean_abs_diff, 'q': q}
 
+
 def get_tf_variable(name, clip):
     value = np.random.normal(0, 0.005 * clip)
     return tf.Variable(value, name=name, dtype=tf.float32)
+
 
 class Control:
     def __init__(self):
         rospy.init_node('control', anonymous=True)
 
         # subscribe to relevant topics
-        self.target_sub = rospy.Subscriber('/target_pos', Float64MultiArray, self.open_control)
+        self.target_sub = rospy.Subscriber(
+            '/target_pos', Float64MultiArray, self.open_control)
 
         # prepare publishers for joint angles
         self.joint_1_pub = rospy.Publisher(
@@ -127,8 +135,8 @@ class Control:
 
     def open_control(self, data, debug=False):
         # we would either use the current estimated joint angles
-        # or reinitialize the join angles to the same initial 
-        # seed for each new target. We empiracally found that, 
+        # or reinitialize the join angles to the same initial
+        # seed for each new target. We empiracally found that,
         # neither of those work the best. Randomly initializing
         # the joint angles around zero, worked the best.
         self._initialize_variables()
