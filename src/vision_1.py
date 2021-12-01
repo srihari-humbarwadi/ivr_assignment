4a6a9bd6b137eae006cf3e499ad17e238bf4c54c
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-12-01 13:21:02 +0530

    linting and doctrings for `vision_1`

--- src/vision_1.py
+++ src/vision_1.py
@@ -3,35 +3,43 @@
 # estimate the positions of joints, as described in 'Joint state estimation - I' in the IVR assignment pdf
 import os
 import sys
-import rospy
+
 import cv2
 import numpy as np
+import rospy
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-from std_msgs.msg    import Float64MultiArray
-from cv_bridge       import CvBridge
+from std_msgs.msg import Float64MultiArray
+
+
+NEARNESS_CONSTANT = 0.4
+
 
 def clip(minimum, maximum, val):
     return max(minimum, min(maximum, val))
 
-# return the argument (x or y) closer to val
+
 def closer_to(val, x, y):
+    """ Return the argument (x or y) closer to val"""
     if abs(x - val) <= abs(y - val):
         return x
     else:
         return y
 
-NEARNESS_CONSTANT = 0.4
+
 def near(val1, val2):
     return abs(val1 - val2) < NEARNESS_CONSTANT
 # TODO: make near_zero use near once no worry of conflicts
+
+
 def near_zero(val):
     return near(val, 0)
 
-# approximate using Langrange interpolation
 def approximate_with_data(ys):
+    """ Approximate using Langrange interpolation """
     def approximator(x):
         total_points = len(ys)
-        total        = 0
+        total = 0
         for i in range(total_points):
             total += ys[i] * \
                 np.product([x - j for j in range(total_points) if j != i]) /  \
@@ -39,8 +47,9 @@ def approximate_with_data(ys):
         return total
     return approximator
 
+
 class Link:
-    # model the link as a vector from joint1 to joint2
+    """ Model the link as a vector from joint1 to joint2 """
     def __init__(self, joint_1, joint_2):
         self.joint_1 = joint_1
         self.joint_2 = joint_2
@@ -60,77 +69,86 @@ class Link:
         norm = np.linalg.norm(vect)
         return vect / norm
 
+
 class Joint:
-    # colour range for opencv binary thresholding
+    """ Abstraction for joints """
     def __init__(self, colour_name, colour_range):
-        self.x            = 0
-        self.y            = 0
-        self.z            = 0
-        self.angle        = 0
-        self.obstructed   = False
-        self.colour_name  = colour_name
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.angle = 0
+        self.obstructed = False
+        self.colour_name = colour_name
         self.colour_range = colour_range
 
         # when not None, used to approximate angles when blobs are obstructed or too close together
         self.approximator = None
         # argument to be passed to the approximator
-        self.approx_arg  = 0
+        self.approx_arg = 0
 
     def copy(self):
         copy = Joint(self.colour_name, self.colour_range)
-        copy.x     = self.x
-        copy.y     = self.y
-        copy.z     = self.z
+        copy.x = self.x
+        copy.y = self.y
+        copy.z = self.z
         copy.angle = self.angle
 
         return copy
+
 
 class vision_1:
     def __init__(self):
         # set up ros nodes/pubs/subs etc.
         rospy.init_node("vision_1", anonymous=True)
-        self.image_1_sub      = rospy.Subscriber('/camera1/robot/image_raw', Image, self.callback_1)
-        self.image_2_sub      = rospy.Subscriber('/camera2/robot/image_raw', Image, self.callback_2)
-        self.joints_est_1_pub = rospy.Publisher("joints_est_1", Float64MultiArray, queue_size=10)
+        self.image_1_sub = rospy.Subscriber(
+            '/camera1/robot/image_raw', Image, self.callback_1)
+        self.image_2_sub = rospy.Subscriber(
+            '/camera2/robot/image_raw', Image, self.callback_2)
+        self.joints_est_1_pub = rospy.Publisher(
+            "joints_est_1", Float64MultiArray, queue_size=10)
         self.bridge = CvBridge()
 
         # TODO: optimize all (hyper-)parameters
         # set up kernel and etc. for blob detection
-        #self.kernel = np.array([ # for more control later
+        # self.kernel = np.array([ # for more control later
         #    [0, 0, 1, 0, 0],
         #    [0, 1, 1, 1, 0],
         #    [1, 1, 1, 1, 1],
         #    [0, 1, 1, 1, 0],
         #    [0, 0, 1, 0, 0]
-        #], np.uint8)
-        self.kernel    = np.full((5, 5), 1, np.uint8)
-        self.no_iter   = 3
-        self.no_joints = 5 #"including end-effector", so off by 1
-        self.no_links  = self.no_joints - 1 # not off by one, does not include "0m link from ground"
+        # ], np.uint8)
+        self.kernel = np.full((5, 5), 1, np.uint8)
+        self.no_iter = 3
+        self.no_joints = 5  # "including end-effector", so off by 1
+        # not off by one, does not include "0m link from ground"
+        self.no_links = self.no_joints - 1
 
         # consider blob blocked (e.g. by link) if its area m00 is below this threshold
         self.obstruct_thres = 1000
 
         # declare joints and their (range of) colours (for opencv thresholding)
-        self.green = Joint('green',  [(0, 100, 0),   (10, 255, 10) ])
-        self.yel1  = Joint('yellow', [(0, 100, 100), (10, 255, 255)])
-        self.yel2  = Joint('yellow', [(0, 100, 100), (10, 255, 255)])
-        self.blue  = Joint('blue',   [(100, 0, 0),   (255, 10, 10) ])
-        self.red   = Joint('red',    [(0, 0, 100),   (10, 10, 255) ])
-        self.joints = [self.green, self.yel1, self.yel2, self.blue, self.red] #index 1-off from pdf
+        self.green = Joint('green',  [(0, 100, 0),   (10, 255, 10)])
+        self.yel1 = Joint('yellow', [(0, 100, 100), (10, 255, 255)])
+        self.yel2 = Joint('yellow', [(0, 100, 100), (10, 255, 255)])
+        self.blue = Joint('blue',   [(100, 0, 0),   (255, 10, 10)])
+        self.red = Joint('red',    [(0, 0, 100),   (10, 10, 255)])
+        self.joints = [self.green, self.yel1, self.yel2,
+                       self.blue, self.red]  # index 1-off from pdf
 
         # keep old copies of joint positions to estimate when blobs are obstructed
         joints_copy = [j.copy() for j in self.joints]
-        self.saved_state_window_size = 50 # includes latest measurement not in prev_joints
+        # includes latest measurement not in prev_joints
+        self.saved_state_window_size = 50
         self.prev_joints = [joints_copy] * (self.saved_state_window_size - 1)
 
         # averages of the states of joints over the copies of joints
-        self.avg_green  = self.green.copy()
-        self.avg_yel1   = self.yel1.copy()
-        self.avg_yel2   = self.yel2.copy()
-        self.avg_blue   = self.blue.copy()
-        self.avg_red    = self.red.copy()
-        self.avg_joints = [self.avg_green, self.avg_yel1, self.avg_yel2, self.avg_blue, self.avg_red]
+        self.avg_green = self.green.copy()
+        self.avg_yel1 = self.yel1.copy()
+        self.avg_yel2 = self.yel2.copy()
+        self.avg_blue = self.blue.copy()
+        self.avg_red = self.red.copy()
+        self.avg_joints = [self.avg_green, self.avg_yel1,
+                           self.avg_yel2, self.avg_blue, self.avg_red]
         # number of saved measurement states to use in average
         self.avg_window_size = 5
 
@@ -163,14 +181,14 @@ class vision_1:
 
         if dump_frames:
             frame_count = self._updates['camera_{}'.format(camera)]
-            
+
             if not os.path.exists(os.path.join(output_dir, str(camera))):
                 os.makedirs(os.path.join(output_dir, str(camera)))
 
             frame_path = os.path.join(output_dir, str(
                 camera), 'frame_{}.png'.format(frame_count))
             cv2.imwrite(frame_path, cv_image)
-        
+
         self._updates['camera_{}'.format(camera)] += 1
 
         # highlight blobs
@@ -187,15 +205,15 @@ class vision_1:
         # update centres, using previous pos. if obscured
         copies = [j.copy() for j in self.joints]
         for i in range(self.no_joints):
-            area       = moments[i]['m00']
+            area = moments[i]['m00']
             if area < self.obstruct_thres:
                 self.joints[i].obstructed = True
                 continue
             self.joints[i].obstructed = False
-            vertical   = int(moments[i]['m01'] / area)
+            vertical = int(moments[i]['m01'] / area)
             horizontal = int(moments[i]['m10'] / area)
 
-            self.joints[i].z     = vertical
+            self.joints[i].z = vertical
             if camera == 1:
                 self.joints[i].y = horizontal
             else:
@@ -205,23 +223,24 @@ class vision_1:
 
         # update averages of joints
         for i in range(self.no_joints):
-            total_x     = self.joints[i].x
-            total_y     = self.joints[i].y
-            total_z     = self.joints[i].z
+            total_x = self.joints[i].x
+            total_y = self.joints[i].y
+            total_z = self.joints[i].z
             total_angle = self.joints[i].angle
             for joints in self.prev_joints[0:self.avg_window_size]:
-                total_x     += joints[i].x
-                total_y     += joints[i].y
-                total_z     += joints[i].z
+                total_x += joints[i].x
+                total_y += joints[i].y
+                total_z += joints[i].z
                 total_angle += joints[i].angle
-            self.avg_joints[i].x     = total_x     / self.avg_window_size
-            self.avg_joints[i].y     = total_y     / self.avg_window_size
-            self.avg_joints[i].z     = total_z     / self.avg_window_size
+            self.avg_joints[i].x = total_x / self.avg_window_size
+            self.avg_joints[i].y = total_y / self.avg_window_size
+            self.avg_joints[i].z = total_z / self.avg_window_size
             self.avg_joints[i].angle = total_angle / self.avg_window_size
-            
+
     def publish_angles(self):
         joints_est = Float64MultiArray()
-        joints_est.data = [self.green.angle, self.yel1.angle, self.yel2.angle, self.blue.angle]
+        joints_est.data = [self.green.angle,
+                           self.yel1.angle, self.yel2.angle, self.blue.angle]
         self.joints_est_1_pub.publish(joints_est)
 
     def update_angles(self):
@@ -234,7 +253,7 @@ class vision_1:
         # update joint 3 angle
         self.yel2.angle = np.arcsin(-link_3_y)
 
-        # update joint 2's angle 
+        # update joint 2's angle
         if not near(np.pi/2, abs(self.yel2.angle)):
             self.yel1.angle = np.arctan2(link_3_x, link_3_z)
         elif not near_zero(self.blue.angle):
@@ -246,9 +265,9 @@ class vision_1:
             self.yel1.angle = np.clip(yel1_angle, -np.pi/2, np.pi/2)
 
         # update joint 4's angle
-        blue_sin = np.cos(self.yel1.angle)*link_4_x - np.sin(self.yel1.angle)*link_4_z
+        blue_sin = np.cos(self.yel1.angle)*link_4_x - \
+            np.sin(self.yel1.angle)*link_4_z
         self.blue.angle = np.arcsin(np.clip(blue_sin, -1, 1))
-
 
 
 def main(args):
@@ -258,6 +277,7 @@ def main(args):
     except KeyboardInterrupt:
         print("Shutting Down")
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main(sys.argv)
